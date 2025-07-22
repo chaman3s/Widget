bf22a60dcc8d9b56df977f0336a65f429bb7eedc
chaman3s <chamanggarwal@gmail.com>
2025-07-22 19:04:10 +0530

    update script file

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const slides = document.getElementById('slides');
   const totalSlides = slides.children.length;
   let index = 0;
-  document.querySelector('.next').addEventListener('click', () => {
-    index = (index + 1) % totalSlides;
-    slides.style.transform = `translateX(-${index * 100}%)`;
-  });
   document.querySelector('.prev').addEventListener('click', () => {
     index = (index - 1 + totalSlides) % totalSlides;
     slides.style.transform = `translateX(-${index * 100}%)`;
